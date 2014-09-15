3da05c0150009d9874b1b935a33e34931d85a4a1
Emma Laroche <E.R.Laroche@gmail.com>
2014-09-15 01:38:14 -0500

    Add if statments to salesClerk() to prevent user from inputting <= 0 integers in value prompt.

--- calculating_tool.js
+++ calculating_tool.js
@@ -1,7 +1,15 @@
 function salesClerk() {
 	var purchasePrice=parseInt(prompt("Enter the value of the item you're purchasing.", "0"), 10);
+	if (purchasePrice == 0) {
+	 alert("There is no sales tax on '0', please input a value greater than '0'.");
+	}
+	if (purchasePrice < 0) {
+		alert ("Please input a value greater than '0'.");
+	}
+	if (purchasePrice > 0) {
 	var taxRate=.08;
 	var taxOnPrice=purchasePrice * taxRate;
 	var total=purchasePrice + taxOnPrice;
 	alert("Your bill is $" + total);
+	}	
 }
