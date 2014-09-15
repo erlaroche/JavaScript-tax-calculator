261c86ab2a60327c385f15de3d82500a5e2dc7a3
Emma Laroche <E.R.Laroche@gmail.com>
2014-09-15 02:55:31 -0500

    Add image.

--- calculating_tool.js
+++ calculating_tool.js
@@ -3,6 +3,7 @@ function salesClerk() {
 	if (purchasePrice <= 0) {
 		alert ("Please input a value greater than '0'.");
 	}
+	//add statement to prevent user from inputing words (prevent NaN popup)
 	else {
 	var taxRate=.08;
 	var taxOnPrice=purchasePrice * taxRate;
