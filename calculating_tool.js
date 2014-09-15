0df3af9990fb82dc64e51dc826b264033b691d73
Emma Laroche <E.R.Laroche@gmail.com>
2014-09-15 01:25:49 -0500

    Add user input prompt, use parseInt to change the user intput into an integer.

--- calculating_tool.js
+++ calculating_tool.js
@@ -1,6 +1,6 @@
 function salesClerk() {
-	var purchasePrice=14.95;
-	var taxRate=.05;
+	var purchasePrice=parseInt(prompt("Enter the value of the item you're purchasing.", "0"), 10);
+	var taxRate=.08;
 	var taxOnPrice=purchasePrice * taxRate;
 	var total=purchasePrice + taxOnPrice;
 	alert("Your bill is $" + total);
