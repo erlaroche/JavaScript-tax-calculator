cb35d717a3d6b45a0cf49c2631371b17b9703185
Emma Laroche <E.R.Laroche@gmail.com>
2014-09-15 02:18:26 -0500

    Refactor 3 if statements into if.. else statement.

--- calculating_tool.js
+++ calculating_tool.js
@@ -1,12 +1,9 @@
 function salesClerk() {
 	var purchasePrice=parseInt(prompt("Enter the value of the item you're purchasing.", "0"), 10);
-	if (purchasePrice == 0) {
-	 alert("There is no sales tax on '0', please input a value greater than '0'.");
-	}
-	if (purchasePrice < 0) {
+	if (purchasePrice <= 0) {
 		alert ("Please input a value greater than '0'.");
 	}
-	if (purchasePrice > 0) {
+	else {
 	var taxRate=.08;
 	var taxOnPrice=purchasePrice * taxRate;
 	var total=purchasePrice + taxOnPrice;
