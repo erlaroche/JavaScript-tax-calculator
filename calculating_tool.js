dcda1c81ee711a9289c400e5a0b3282e2672d0b2
Emma Laroche <E.R.Laroche@gmail.com>
2014-09-23 23:37:27 -0500

    Elvis refactors my JavaScript and adds a statment to prevent user from inputing words.

--- calculating_tool.js
+++ calculating_tool.js
@@ -1,4 +1,5 @@
 function salesClerk() {
+	/*
 	var purchasePrice=parseFloat(prompt("Enter the value of the item you're purchasing.", "0"), 10);
 	if (purchasePrice <= 0) {
 		alert ("Please input a value greater than '0'.");
@@ -9,5 +10,20 @@ function salesClerk() {
 	var taxOnPrice=purchasePrice * taxRate;
 	var total=purchasePrice + taxOnPrice;
 	alert("Your bill is $" + Math.round(total*Math.pow(10,2))/Math.pow(10,2));
+	}	
+}
+*/
+    var Tax_Array = []; // Declare new Array. Same as 'new Array();'
+	var purchasePrice=parseFloat(prompt("Enter the value of the item you're purchasing.", "0"), 10);
+	if (purchasePrice <= 0) {
+		alert ("Please input a value greater than '0'.");
+	}
+	//add statement to prevent user from inputing words (prevent NaN popup)
+	else {
+	Tax_Array[0]=.08 * purchasePrice;
+	Tax_Array[1]=purchasePrice + Tax_Array[0];
+	alert("Your bill is $" + Math['ro' 
+
+		+ 'und'](Tax_Array[1]*Math.pow(10,2))/Math.pow(10,2));
 	}	
 }
